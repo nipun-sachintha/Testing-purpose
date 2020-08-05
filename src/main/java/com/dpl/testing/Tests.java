069c2e181e59971985bc098d63b02ee849f2aa1a
nipun-sachintha <nipunsachinthaabey@gmail.com>
2020-08-05 14:30:36 +0530

    fourth commit

--- src/main/java/com/dpl/testing/Tests.java
+++ src/main/java/com/dpl/testing/Tests.java
@@ -4,7 +4,7 @@ public class Tests {
 
     public static void main(String[]args){
 
-        System.out.println("as commit change get cloned");
+        System.out.println("lets see whether this commit works");
     }
 
 }
