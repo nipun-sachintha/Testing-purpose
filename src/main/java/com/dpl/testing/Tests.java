92ef63ccb65c947ceff8e75be8c4629ddacee6d4
nipun-sachintha <nipunsachinthaabey@gmail.com>
2020-08-05 12:13:26 +0530

    third commit

--- src/main/java/com/dpl/testing/Tests.java
+++ src/main/java/com/dpl/testing/Tests.java
@@ -3,7 +3,8 @@ package com.dpl.testing;
 public class Tests {
 
     public static void main(String[]args){
-        System.out.println("hello");
+
+        System.out.println("as commit change get cloned");
     }
 
 }
